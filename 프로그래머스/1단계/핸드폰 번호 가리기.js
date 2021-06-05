76945b1599e696554d507bf87e1246aa3b6815a3
krim45 <82519180+krim45@users.noreply.github.com>
2021-06-05 21:13:47 +0900

    Update 핸드폰 번호 가리기.js

--- 프로그래머스/1단계/핸드폰 번호 가리기.js
+++ 프로그래머스/1단계/핸드폰 번호 가리기.js
@@ -1,8 +1,17 @@
 function solution(phone_number) {
     var answer = '';
+    answer += phone_number.slice(-4);
+    for (let i = 0;i < phone_number.length - 4;i++) {
+        answer = '*' + answer;
+    }
+    return answer;
+}
+
+function solution(phone_number) {
+    var answer = '';
     let psplit = phone_number.split('');
     for (let i = 0;i < phone_number.length - 4;i++) {
-    psplit[i] = '*';
+       psplit[i] = '*';
     }
     answer = psplit.toString().replace(/,/g,''); //replace 정규표현식
     return answer;
