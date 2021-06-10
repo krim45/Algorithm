b21c9103de3558b69af30d0ff1831e04aa9f6c5b
krim45 <82519180+krim45@users.noreply.github.com>
2021-06-10 22:04:39 +0900

    Update 자릿수 더하기.js

--- 프로그래머스/1단계/자릿수 더하기.js
+++ 프로그래머스/1단계/자릿수 더하기.js
@@ -8,7 +8,7 @@ function solution(n) {
     return answer;
 }
 
-// 숫자로 풀기
+// 숫자로 풀기 1
 function solution(n) {
     var answer = 0;
     let b = n;
@@ -16,6 +16,17 @@ function solution(n) {
     for (let i = 0; i < (n + '').length; i++) {
         a = Math.floor(b / Math.pow(10, (n + '').length - i - 1));
         b = b - (a * Math.pow(10, (n + '').length - i - 1));
+        answer += a;
+    }
+    return answer;
+}
+
+// 숫자로 풀기 2
+function solution(n) {
+    var answer = 0;
+    while (n > 0) {
+    let a = n % 10;
+        n = Math.floor(n / 10);
         answer += a;
     }
     return answer;
