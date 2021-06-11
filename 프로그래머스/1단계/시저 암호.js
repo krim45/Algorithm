6c9a28f26e36921e760bda8ccb63cd2bee4cc871
krim45 <82519180+krim45@users.noreply.github.com>
2021-06-11 21:01:32 +0900

    Update 시저 암호.js

--- 프로그래머스/1단계/시저 암호.js
+++ 프로그래머스/1단계/시저 암호.js
@@ -19,3 +19,20 @@ function solution(s, n) {
     }
     return answer;
 }
+
+// 좀더 간결하게
+function solution(s, n) {
+    var answer = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            answer += ' '
+        } 
+        else if (s.charCodeAt(i) < 91) {
+            answer += String.fromCharCode((s.charCodeAt(i) - 65 + n) % 26 + 65);
+        }  
+        else {
+            answer += String.fromCharCode((s.charCodeAt(i) - 97 + n) % 26 + 97);    
+        }
+    }  
+    return answer;
+}
