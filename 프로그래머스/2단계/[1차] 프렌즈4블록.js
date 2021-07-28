8345ab9012364ca094025befb03cec335572bb5a
krim45 <82519180+krim45@users.noreply.github.com>
2021-07-28 20:08:06 +0900

    Update [1차] 프렌즈4블록.js

--- 프로그래머스/2단계/[1차] 프렌즈4블록.js
+++ 프로그래머스/2단계/[1차] 프렌즈4블록.js
@@ -1,73 +1,66 @@
 function solution(m, n, board) {
     var answer = 0;
-    let count = m/2 + 1;
-    var tmp;
+    let tmp;
     board = board.map(v => v.split(""));
     
-    function boom(a, b) {
-        if (board[a][b] !== 0) {
-            if (board[a][b][0] === board[a + 1][b][0] && board[a][b][0] === board[a + 1][b + 1][0] && board[a][b][0] === board[a][b + 1][0]) {
-                if (board[a][b][1] !== "0") board[a][b] += "0";
-                if (board[a+1][b][1] !== "0") board[a + 1][b] += "0";
-                if (board[a][b+1][1] !== "0") board[a][b + 1] += "0";
-                if (board[a+1][b+1][1] !== "0") board[a + 1][b + 1] += "0";
-            }
-        }
-    }
-    
     function check() {
-        for (let i = 0; i < board.length - 1; i++) {
-            for (let j = 0; j < board[i].length - 1; j++) {
-                boom(i,j);
+        for (let i = 0; i < board.length-1; i++) {
+            for (let j = 0; j < board[i].length-1; j++) {
+                if (board[i][j] !== 0) {
+                    if (board[i][j][0] === board[i+1][j][0] && board[i][j][0] === board[i+1][j+1][0] && board[i][j][0] === board[i][j+1][0]) {
+                        if (board[i][j][1] !== "0") board[i][j] += "0";
+                        if (board[i+1][j][1] !== "0") board[i + 1][j] += "0";
+                        if (board[i][j+1][1] !== "0") board[i][j + 1] += "0";
+                        if (board[i+1][j+1][1] !== "0") board[i + 1][j + 1] += "0";
+                    }
+                }
             }
         }
     }
     
     function del() {
+        let count = 0;
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
                 if (board[i][j][1] === "0") {
                     board[i][j] = 0;
-                    answer++;
+                    count++;
                 }
             }
         }
+        return count;
     }
     
     function findNotZero(a, b) {
-        if (a-1 >= 0) {
-            if (board[a-1][b] === 0) {
-                if (a-1 > 0) {
-                    findNotZero(a-1, b);
-                }
-            } else if (board[a-1][b] !== undefined) {
-                tmp = board[a-1][b];
-                board[a-1][b] = 0;
-            }
+        if (board[a-1][b] === 0 && a-2 >= 0) {
+            return findNotZero(a-1,b);
+        } else {
+            tmp = board[a-1][b];
+            board[a-1][b] = 0;
         }
     }
     
     function down() {
-        for (let i = board.length-1; i > -1; i--) {
+        for (let i = board.length -1; i > 0; i--) {
             for (let j = 0; j < board[i].length; j++) {
-                if (board[i][j] === 0) {
-                    findNotZero(i, j);
-                    if (tmp !== undefined) {
-                        board[i][j] = tmp;
-                    }
-                    tmp = 0;
-                }
+                   if (board[i][j] === 0) {
+                       findNotZero(i,j);
+                       board[i][j] = tmp;
+                       tmp = 1;
+                   }
             }
         }
     }
     
-    while (count > 0) {
+    while (true) {
         check();
-        del();
+        let stop = del();
+        if (stop === 0) {
+            break;
+        }
+        answer += stop;
         down();
-        count--;
     }
-    
     
     return answer;
 }
