7d12b0e5fcb25dd732f5ec29eecc2380f82c0fb0
krim45 <krim45@naver.com>
2021-10-02 01:32:36 +0900

    bfs로 시간 줄이기, shift는 연산이 느리다.

--- 백준/N7576.js
+++ 백준/N7576.js
@@ -2,68 +2,47 @@ const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('
 const [M, N] = input.shift();
 
 function solution(input) {
-  const graph = Array.from(Array(N), () => new Array(M).fill(false));
+  const visited = Array.from(Array(N), () => new Array(M).fill(false));
   const x1 = [-1, 1, 0, 0];
   const y1 = [0, 0, -1, 1];
   const queue = [];
   let day = 0;
+  let idx = 0;
   let count = 0;
-  let tmp = 1000001;
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      if (input[i][j] === 1) {
-        graph[i][j] = true;
+      if (input[i][j] === 1 && !visited[i][j]) {
+        visited[i][j] = true;
+        queue.push([i, j, day]);
+      }
+
+      if (input[i][j] === -1 && !visited[i][j]) {
+        visited[i][j] = true;
         count++
       }
-      if (input[i][j] === -1) {
-        count++
+    }
+  }
+
+  while (queue.length > idx) {
+    const [startY, startX, newDay] = queue[idx++];
+    day = newDay;
+    count++;
+
+    for (let i = 0; i < 4; i++) {
+      const [Y, X] = [startY + y1[i], startX + x1[i]];
+      if (0 <= Y && Y < N && 0 <= X && X < M && !visited[Y][X]) {
+        queue.push([Y, X, newDay + 1]);
+        visited[Y][X] = true;
       }
     }
   }
 
-  function bfs(y, x) {
-    graph[y][x] = true;
-    queue.push([y, x]);
-
-    while (queue.length > 0) {
-      const start = queue.shift();
-      for (let i = 0; i < 4; i++) {
-        const [Y, X] = [y + y1[i], x + x1[i]];
-
-        if (0 <= Y && Y < N && 0 <= X && X < M && input[Y][X] !== -1 && graph[Y][X] === false) {
-          graph[Y][X] = true;
-          queue.push([Y, X]);
-          count++
-        }
-      }
-    }
+  if (count !== M * N) {
+    console.log(-1);
+    return;
   }
-
-  while (count < M * N) {
-    tmp = count;
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (input[i][j] === 1) {
-          bfs(i, j);
-        }
-      }
-    }
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (graph[i][j] === true && input[i][j] !== 1) {
-          input[i][j] = 1;
-        }
-      }
-    }
-    day++;
-
-    if (tmp === count) {
-      console.log(-1);
-      return;
-    }
-  }
-  console.log(day);
+  console.log(day)
 }
 
 solution(input);
