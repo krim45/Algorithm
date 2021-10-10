49b0165d81fec6e9f945248a5f26b5d4b199dae3
krim45 <krim45@naver.com>
2021-10-10 23:20:47 +0900

    Create N11286.js

---
+++ 백준/N11286.js
@@ -0,0 +1,92 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+const N = input.shift();
+const result = [];
+const heap = new Array(100001);
+let heapSize = 0;
+
+const swap = (idx1, idx2) => {
+  let tmp = heap[idx1];
+  heap[idx1] = heap[idx2];
+  heap[idx2] = tmp;
+}
+
+// 상향식
+const insert = (value) => {
+  heap[++heapSize] = value;
+  let idx = heapSize;
+
+  while (idx !== 1) {
+    let parentIdx = Math.floor(idx / 2)
+
+    if (Math.abs(heap[idx]) < Math.abs(heap[parentIdx])) {
+      swap(idx, parentIdx);
+      idx = parentIdx;
+    } else if (Math.abs(heap[idx]) === Math.abs(heap[parentIdx])) {
+      if (heap[idx] < heap[parentIdx]) {
+        swap(idx, parentIdx);
+        idx = parentIdx;
+      } else {
+        break;
+      }
+    }
+    else {
+      break;
+    }
+  }
+}
+
+// 하향식
+const getMin = () => {
+  if (!heapSize) return 0
+  let result = heap[1];
+  heap[1] = heap[heapSize];
+  heap[heapSize] = null;
+  heapSize--;
+  let idx = 1;
+
+  // 최소 heap 추출 후 heap 재구성
+  let childIdx;
+  while (idx <= heapSize) {
+    // 왼쪽 자식 노드 존재
+    if (idx * 2 <= heapSize) {
+      childIdx = idx * 2;
+      // 오른쪽 자식 노드 존재
+      if (idx * 2 + 1 <= heapSize) {
+        if (Math.abs(heap[idx * 2]) > Math.abs(heap[idx * 2 + 1])) {
+          childIdx = idx * 2 + 1;
+        }
+        else if (Math.abs(heap[idx * 2]) === Math.abs(heap[idx * 2 + 1])) {
+          if (heap[idx * 2] > heap[idx * 2 + 1]) {
+            childIdx = idx * 2 + 1;
+          }
+        }
+      }
+    }
+
+    if (Math.abs(heap[idx]) > Math.abs(heap[childIdx])) {
+      swap(idx, childIdx);
+      idx = childIdx;
+    } else if (Math.abs(heap[idx]) === Math.abs(heap[childIdx])) {
+      if (heap[idx] > heap[childIdx]) {
+        swap(idx, childIdx);
+        idx = childIdx;
+      } else {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  return result
+}
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i]) {
+    insert(input[i])
+  } else {
+    result.push(getMin());
+  }
+}
+
+console.log(result.join('\n'))
