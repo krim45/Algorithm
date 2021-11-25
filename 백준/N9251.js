f98ed18539dde92ee282dc0fc0b1cc7eac570bcd
krim45 <krim45@naver.com>
2021-11-25 23:46:24 +0900

    Create N9251.js

---
+++ 백준/N9251.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+const [str1, str2] = input;
+
+const LCS = function (str1, str2) {
+  const M = str1.length;
+  const N = str2.length;
+  const dp = new Array(M + 1).fill(0).map(() => new Array(N + 1).fill(0));
+
+  for (let i = 0; i <= M; i++) {
+    for (let j = 0; j <= N; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 0
+      } else if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[M][N]
+};
+
+console.log(LCS(str1, str2));
