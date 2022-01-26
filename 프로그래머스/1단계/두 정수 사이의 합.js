58d35052ef85332a248a33eadeb9c646d7b4339e
김경봉 <krim45@naver.com>
2022-01-26 18:04:02 +0900

    Update 두 정수 사이의 합.js

--- 프로그래머스/1단계/두 정수 사이의 합.js
+++ 프로그래머스/1단계/두 정수 사이의 합.js
@@ -1,14 +1,18 @@
 function solution(a, b) {
-    var answer = 0;
-    let max = Math.max(a, b);
-    let min = Math.min(a, b);
-    for (let i = min; i <= max; i++) {
-        answer += i;
-    }
-    return answer;
+  let answer = 0;
+  let [min, max] = a > b ? [b, a] : [a, b];
+    
+  for (let i = min; i <= max; i++) {
+      answer += i;
+  }
+    
+  return answer;
 }
 
-// 등차수열 개념 (양 끝의 합 * 양 끝의 합의 개수)/ 2 
+// 가우스의 덧셈 (양 끝의 합 * 양 끝의 합의 개수)/ 2 
 function solution(a, b) {
-    return (a+b)*(Math.abs(b-a)+1)/2;
+  let answer = 0;
+  let [min, max] = a > b ? [b, a] : [a, b];
+  
+  return (min + max) * (max - min + 1) / 2
 }
