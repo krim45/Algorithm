cd321a5c6f1c5d24bd7963ad7d0fa4e6f2de761e
김경봉 <krim45@naver.com>
2022-01-26 21:59:23 +0900

    Update 실패율.js

--- 프로그래머스/1단계/실패율.js
+++ 프로그래머스/1단계/실패율.js
@@ -31,3 +31,45 @@ function solution(N, stages) {
     }
     return arr;
 }
+
+//
+function solution(N, stages) {
+    const board = new Array(N).fill(0);
+    const challenger = new Array(N).fill(0);
+    const failure = new Array(N).fill(0);
+    
+    for (let i = 0; i < stages.length; i++)  {
+        const stage = stages[i];
+        if (stage <= N) failure[stage - 1] += 1;
+        
+        for (let j = 0; j < stage; j++) {
+            if (j < N) challenger[j] += 1;
+        }
+    }
+    
+    const result = board.map((v, i) => {
+        const rate = challenger[i] !== 0 ? failure[i] / challenger[i] : 0;
+        
+        return [i + 1, rate]
+    }).sort((a, b) => b[1] - a[1]);
+    
+    
+    return result.map(v => v[0]);
+}
+
+//
+function solution(N, stages) {
+    const board = [];
+    
+    for (let stage = 1; stage <= N; stage++)  {
+        const challenger = stages.filter(value => value >= stage).length;
+        const current = stages.filter(value => value === stage).length;
+        const failRate = challenger === 0 ? 0 : current / challenger;
+        
+        board.push([stage, failRate]);
+    }
+    
+    board.sort((a, b) => a[1] === b[1] ? a[0] - b[0] : b[1] - a[1]);
+        
+    return board.map(v => v[0]);
+}
